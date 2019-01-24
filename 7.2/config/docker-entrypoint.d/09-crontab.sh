19435c49e7b878bf81163991a702fcd94d732ff5
Leonid Makarov <leonid.makarov@ffwagency.com>
2019-01-24 12:44:36 -0800

    Updated cron settings
    - Removed CRONTAB parsing. Using the variable value as is

--- 7.2/config/docker-entrypoint.d/09-crontab.sh
+++ 7.2/config/docker-entrypoint.d/09-crontab.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
-# This script generates crontab settings passed the CRONTAB environment variable.
-# Settings are passed as a text blob. Individual lines should be separated '; ' (semicolon-space).
+# This script generates crontab settings for the docker user from the CRONTAB environment variable.
 #
 # Example:
 #
-# CRONTAB='*/1 * * * * echo "$(date): I run every minute" >> /tmp/dummy.txt; */2 * * * * echo "$(date): I run every two minutes" >> /tmp/dummy.txt'
-#
-# will parsed and written to docker user's crontab as
-#
-# */1 * * * * touch /tmp/dummy.txt; echo "$(date): I run every minute" >> /tmp/dummy.txt
-# */2 * * * * touch /tmp/dummy.txt; echo "$(date): I run every two minutes" >> /tmp/dummy.txt
+# CRONTAB='
+# */1 * * * * echo "$(date): I run every minute" >> /tmp/dummy.txt
+# */2 * * * * echo "$(date): I run every two minutes" >> /tmp/dummy.txt
+# '
 #
 
+# Configure crontabs
 if [[ "${CRONTAB}" != "" ]]; then
 	echo_debug "Applying crontab settings..."
-	# Configure crontabs. '; ' is used as a line separator
-	echo -e "${CRONTAB}" | sed 's/; /\n/g' | crontab -u docker -
-	# List configured crontabs
+	echo -e "${CRONTAB}" | crontab -u docker -
+	# Print crontab for verification
 	echo_debug "\n---\n$(crontab -l -u docker)\n---"
 fi
